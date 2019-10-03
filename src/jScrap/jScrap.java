65f2c22ccf5a4138a09145b60acfd10f2c41a8de
Martin Hronek <martin.hronek@cz.ibm.com>
2019-10-03 14:32:23 +0200

    Initial upload.

---
+++ src/jScrap/jScrap.java
@@ -0,0 +1,20 @@
+package jScrap;
+
+import java.io.File;
+
+public class jScrap {
+	static private File inputJSON;
+	
+	public jScrap(File inputJSON) {
+		jScrap.inputJSON = inputJSON;
+	}
+
+
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
